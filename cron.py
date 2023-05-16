074c226147e2dbaf040e7859f26992406ec3621b
an19352 <piyushbajaj71@gmail.com>
2023-05-16 17:19:13 +0100

    initial commit

--- cron.py
+++ cron.py
@@ -5,6 +5,7 @@ import sys
 import traceback
 
 import crawler
+import tiktoken
 import mailgun
 import responder
 import solution_manager
@@ -19,76 +20,90 @@ def main(crawl=True):
     # Handle incoming emails
 
     email_filenames = os.listdir(MAIL_SAVE_DIR)
+    count = 0
 
     for email_filename in email_filenames:
-        try:
-            print(f"Handling {email_filename}")
-            email_path = os.path.join(MAIL_SAVE_DIR, email_filename)
-            with open(email_path, "r", encoding="utf8") as f:
-                email_obj = json.load(f)
-
-            text = email_obj["content"]
-            subject = str(email_obj["title"])
-            if not subject.startswith("Re:"):
-                subject = "Re: " + subject
-            scam_email = email_obj["from"]
-
-            if "bait_email" not in email_obj:
-                # Email just crawled
-
-                if solution_manager.scam_exists(scam_email):
-                    print("This crawled email has been replied, ignoring")
-                    os.remove(email_path)
-                    continue
-                    # pass
-
-                archive(True, scam_email, "CRAWLER", email_obj["title"], text)
-
-                print("This email is just crawled, using random replier")
-                replier = responder.get_replier_randomly()
-                bait_email = solution_manager.gen_new_addr(scam_email, replier.name)
-                stored_info = solution_manager.get_stored_info(bait_email, scam_email)
-            else:
-                bait_email = email_obj["bait_email"]
-                stored_info = solution_manager.get_stored_info(bait_email, scam_email)
-
-                if stored_info is None:
-                    print(f"Cannot found replier for {bait_email}")
-                    os.remove(email_path)
-                    continue
-
-                print(f"Found selected replier {stored_info.sol}")
-                replier = responder.get_replier_by_name(stored_info.sol)
-                if replier is None:
-                    print("Replier Sol_name not found")
-                    os.remove(email_path)
-                    continue
-
+        if count < 51:
             try:
-                res_text = replier.get_reply_by_his(scam_email)
+                print(f"Handling {email_filename}")
+                email_path = os.path.join(MAIL_SAVE_DIR, email_filename)
+                with open(email_path, "r", encoding="utf8") as f:
+                    email_obj = json.load(f)
+
+                text = email_obj["content"]
+
+                encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+                num_tokens = len(encoding.encode(text))
+                if num_tokens > 900:
+                    print("This email is too long")
+                    os.remove(email_path)
+                    continue
+
+                subject = str(email_obj["title"])
+                if not subject.startswith("Re:"):
+                    subject = "Re: " + subject
+                scam_email = email_obj["from"]
+
+                if "bait_email" not in email_obj:
+                    # Email just crawled
+
+                    if solution_manager.scam_exists(scam_email):
+                        print("This crawled email has been replied, ignoring")
+                        os.remove(email_path)
+                        continue
+                        # pass
+
+                    archive(True, scam_email, "CRAWLER", email_obj["title"], text)
+
+                    print("This email is just crawled, using random replier")
+                    replier = responder.get_replier_randomly()
+                    bait_email = solution_manager.gen_new_addr(scam_email, replier.name)
+                    stored_info = solution_manager.get_stored_info(bait_email, scam_email)
+                else:
+                    bait_email = email_obj["bait_email"]
+                    stored_info = solution_manager.get_stored_info(bait_email, scam_email)
+
+                    if stored_info is None:
+                        print(f"Cannot found replier for {bait_email}")
+                        os.remove(email_path)
+                        continue
+
+                    print(f"Found selected replier {stored_info.sol}")
+                    replier = responder.get_replier_by_name(stored_info.sol)
+                    if replier is None:
+                        print("Replier Sol_name not found")
+                        os.remove(email_path)
+                        continue
+
+                try:
+                    # res_text = replier.get_reply_by_his(scam_email)
+                    res_text = replier.get_reply(text)
+                except Exception as e:
+                    print("GENERATING ERROR")
+                    print(e)
+                    print(traceback.format_exc())
+                    print("Due to CUDA Error, stopping whole sequence")
+                    return
+
+                    # Add Signature
+                res_text += f"\n\nBest wishes,\n{stored_info.username}"
+
+                send_result = mailgun.send_email(stored_info.username, stored_info.addr, scam_email, subject, res_text)
+                if send_result:
+                    print(f"Successfully sent response to {scam_email}")
+                    count += 1
+
+                    # Move from queued to handled dir
+                    if not os.path.exists(MAIL_HANDLED_DIR):
+                        os.makedirs(MAIL_HANDLED_DIR)
+                    shutil.move(email_path, os.path.join(MAIL_HANDLED_DIR, email_filename))
+
+                    archive(False, scam_email, bait_email, subject, res_text)
             except Exception as e:
-                print("GENERATING ERROR")
                 print(e)
                 print(traceback.format_exc())
-                print("Due to CUDA Error, stopping whole sequence")
-                return
-
-                # Add Signature
-            res_text += f"\nBest,\n{stored_info.username}"
-
-            send_result = mailgun.send_email(stored_info.username, stored_info.addr, scam_email, subject, res_text)
-            if send_result:
-                print(f"Successfully sent response to {scam_email}")
-
-                # Move to queued
-                if not os.path.exists(MAIL_HANDLED_DIR):
-                    os.makedirs(MAIL_HANDLED_DIR)
-                shutil.move(email_path, os.path.join(MAIL_HANDLED_DIR, email_filename))
-
-                archive(False, scam_email, bait_email, subject, res_text)
-        except Exception as e:
-            print(e)
-            print(traceback.format_exc())
+        else:
+            break
 
 
 if __name__ == '__main__':
@@ -103,3 +118,5 @@ if __name__ == '__main__':
     main(crawl=arg_crawl)
 
     os.remove("./lock")
+
+# main()
---
+++ secret.py
@@ -0,0 +1,20 @@
+# MAIL
+API_KEY = ""# "YOUR_MAILGUN_API_KEY"
+API_BASE_URL = ""  # "YOUR_MAILGUN_API_BASE_URL"
+DOMAIN_NAME = ""  # "YOUR_SERVER_DOMAIN_NAME"
+MAIL_SAVE_DIR = "./emails/queued" # crawled and received emails
+MAIL_ARCHIVE_DIR = "./emails/archive" #archive
+MAIL_HANDLED_DIR = "./emails/handled" # emails replied to
+ADDR_SOL_PATH = "./emails/record.json" # stores email addresses and names and strategies used
+# MODEL PATHS
+MODEL_HISTORY_PATH = "./models/history.json" #list of responders and times used
+CLASSIFIER_PATH = "./models/classifier/final-model.pt"
+OPENAI_API_KEY = ""
+FILENAME1 = "./data/eliza_dane_green_days.json"
+FILENAME2 = "./data/tushie-blessing.json"
+FILENAME3 = "./data/noogie_california_dreamin.json"
+TEMPLATES_DIR = "./responder/templates"
+# CRAWLER CONF
+CRAWLER_PROG_DIR = "./cache" # has crawled cache
+MAX_PAGE_SL = 2
+MAX_PAGE_SS = 7
